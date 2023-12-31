a17f7e23396911b7de17e7e51b4ff723a2bbdd5c
suarezKevin <suarezk453@gmail.com>
2023-12-31 18:08:37 -0500

    Astro deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://suarezKevin.github.io',
+  base: '/astro-landing-delete',
 });
